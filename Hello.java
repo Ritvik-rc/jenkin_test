0d845411c82645e519798823f560eb7d1ef501c5
Ritvik-rc <77318928+Ritvik-rc@users.noreply.github.com>
2021-01-28 19:30:30 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,5 @@ public class Hello
     public static void main(String[] args) {
         System.out.println("Welcome !! Happy Coding :)");
         System.out.println("I HAVE SUCCESSFULLY TESTED WEBHOOK");
-        System.out.println("I HAVE SUCCESSFULLY TESTED WEBHOOK AFTER CLOSING CMD");
     }
 }
